86b3a2eb514bc7f45bf6845b0f0086c3931369ab
Nurettin Şen <nurie487@gmail.com>
2022-03-31 15:14:10 +0300

    Add sidebar and chat

--- client/src/App.js
+++ client/src/App.js
@@ -2,87 +2,23 @@ import React,{useState,useEffect,useContext} from 'react'
 import { useDispatch } from 'react-redux';
 import {BrowserRouter as Router, Routes,Route,Navigate, BrowserRouter } from "react-router-dom"
 
-import { makeStyles } from '@material-ui/core/styles'
-
-import {
-    CssBaseline,
-    Container,
-    Grid,
-  
-    Toolbar,
-    Typography,
-    Button,
-    IconButton,
-    Avatar,
-    Chip,
-    Badge,
-} from "@material-ui/core"
-
-import Room from "./components/Room"
-import PageNotFound from "./components/PageNotFound"
-
-
-
-  const useStyle = makeStyles((theme) => ({
-    root:{
-        flexGrow:1,
-  
-    },
-    menuButton:{
-        marginRight:theme.spacing(2)
-  
-    },
-    title:{
-        flexGrow:1,
-        
-    },
-    container:{
-  marginTop:theme.spacing(3)
-    }
-  }))
-  
-
-
+import "./App.css"
+import Sidebar from './sidebar';
+import Chat from './Chat';
 
 
 const App = () => {
 
  
-  
-  const classes = useStyle();
-
     return (
-        <>
-        <CssBaseline/>
-
-        <Container maxWidth="lg"  >
-     
-     
-
-<Grid container className={classes.container}>
-    <Grid item xs={12}>
-  
-   <Router>
-   
-      <Routes>
-     
-      <Route  path="/" element={<Room  />} />
-     
-         
-            <Route  path="*" element={<PageNotFound />} />
-          
-           
-            </Routes>
-           
-            </Router>
-        
-
+        <div className='app'>
+         <div className='app__body'>
+         <Sidebar />
+         <Chat />
+         </div>
       
-    </Grid>
-</Grid>
-        </Container>
-     
-        </>
+  
+        </div>
     )
 }
 
