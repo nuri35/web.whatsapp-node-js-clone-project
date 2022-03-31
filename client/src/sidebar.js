02964e51838da7565c74ed36f203e0788754e81c
Nurettin Şen <nurie487@gmail.com>
2022-03-31 21:26:53 +0300

    Add toolbar

--- client/src/sidebar.js
+++ client/src/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Sidebar.css"
 import ChatIcon from '@mui/icons-material/Chat';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -6,8 +6,30 @@ import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Avatar,IconButton} from "@material-ui/core"
 import SidebarChat from "./SidebarChat"
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+const pages = ['Yeni grup', 'Çıkış'];
+import Toolbar from '@mui/material/Toolbar';
+
 
 function sidebar() {
+
+  
+
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
   return (
     <div className='sidebar'>
         <div className='sidebar__header'>
@@ -19,10 +41,38 @@ function sidebar() {
       </IconButton>
 
       
+      <Toolbar disableGutters>
+      <Box >
+      <IconButton  
+        onClick={handleOpenNavMenu}
 
-      <IconButton>
+         >
         <MoreVertIcon />
       </IconButton>
+      <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+             
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            </Box>
+            </Toolbar>
       </div>
 
         </div>
@@ -30,7 +80,7 @@ function sidebar() {
         <div className='sidebar__search'>
       <div className='sidebar__searchContainer'>
 
-    <SearchOutlinedIcon />
+    <SearchOutlinedIcon fontSize='large'/>
     <input placeholder='Search ' type="text" />
       </div>
 
