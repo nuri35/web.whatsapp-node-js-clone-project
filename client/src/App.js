811f3bfdd47fad110fbe7d9a5eaeebcc9c68e283
Nurettin Şen <nurie487@gmail.com>
2022-03-31 21:24:07 +0300

    Update route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
 import React,{useState,useEffect,useContext} from 'react'
 import { useDispatch } from 'react-redux';
-import {BrowserRouter as Router, Routes,Route,Navigate, BrowserRouter } from "react-router-dom"
+import {BrowserRouter as Router, Routes,Route,Navigate,Switch } from "react-router-dom"
 
 import "./App.css"
 import Sidebar from './sidebar';
 import Chat from './Chat';
+import Login from "./components/Login"
+
 
 
 const App = () => {
 
+  let auth = true
  
     return (
         <div className='app'>
          <div className='app__body'>
-         <Sidebar />
-         <Chat />
+
+         <Router>
+   
+   <Routes>
+     {auth ? 
+     <Route  path="/"  element={<><Sidebar/><Chat/></>} />
+    :
+    <Route  path="/" element={<Login />} />      
+   
+    }
+       
+       
+        
+         </Routes>
+        
+         </Router>
+
+        
+        
          </div>
       
   
