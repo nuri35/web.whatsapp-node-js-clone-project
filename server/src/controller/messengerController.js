7212c7362650205c71aa1b31b9bef28d1b2a18fd
Nurettin Şen <nurie487@gmail.com>
2022-04-17 19:59:14 +0300

    İnsert messageSendİmage

--- server/src/controller/messengerController.js
+++ server/src/controller/messengerController.js
@@ -1,6 +1,6 @@
 var client = require('./../db/redis_config');
 const shortid = require('shortid');
-
+const _ = require("lodash")
 
 const messageSend = async(req,res,next)=>{
 
@@ -15,7 +15,7 @@ const messageSend = async(req,res,next)=>{
         })
 
         );
-        res.status(200).json({success:true,message:"success message"})
+        res.status(200).json({message:{message:req.body.messageContent,when:Date.now(),senderId:req.body.senderId}})
        
 
     }catch(err){
@@ -28,33 +28,54 @@ const messageSend = async(req,res,next)=>{
 const getMessageByUser = async(req,res,next)=>{
 //redis get 
     try{
-        
-
+        let messagebyuser = []
         const getMessage = await client.hVals(req.params.id);
-      
-           
-        const message = getMessage.filter(m => {
-          let parseMessage = JSON.parse(m)
-            if(parseMessage.senderId == req.user.id){
-                return parseMessage
+        
+         getMessage.map((m)=>{
+          let messParse = JSON.parse(m)
+            return messParse
+        }).filter(mfilter =>{
+            if(mfilter.senderId == req.user.id){
+               
+                    messagebyuser.push(mfilter)  
             }
-          
-        } );
+        })
+        
+        const getReceiveMe = await client.hVals(req.user.id);
 
-       res.status(200).json({message})
+         getReceiveMe.map((m)=>{
+          let  parseReceive = JSON.parse(m)
+            return parseReceive
+        }).filter(mfilt=>{
+           
+            if(mfilt.senderId == req.params.id){
+                messagebyuser.push(mfilt)
+            }
+        })
+        
+    res.status(200).json({message:_.orderBy(messagebyuser,"when","asc")})
     
 
     }catch(err){
-        console.log(err)
+      
+        res.status(500).json({err:{errorMessage:"Internal server err "}})
+    }
+}
+
+const messageSendİmage = async(req,res,next)=>{
+
+    try{
+      
+        console.log(req.body)
+    }catch(err){
+       
         res.status(500).json({err:{errorMessage:"Internal server err "}})
     }
 }
 
 
-
-
 module.exports = {
    messageSend,
    getMessageByUser,
-    
+   messageSendİmage,
 }
--- server/src/router/messengerRouter.js
+++ server/src/router/messengerRouter.js
@@ -6,6 +6,6 @@ const messengerController = require("../controller/messengerController")
 
 router.post("/sendMessage",messengerController.messageSend)
 router.get("/getMessage/:id",messengerController.getMessageByUser)
-
+router.post("/imageMessageSend",messengerController.messageSendİmage)
 
 module.exports=router
