f6a474ff6abf437a0f42043b0a0dc46229871b24
Nurettin Şen <nurie487@gmail.com>
2022-04-28 21:03:37 +0300

    Add realtime user and message

--- socket/index.js
+++ socket/index.js
@@ -29,18 +29,47 @@ const userRemove = (socketId) =>{
   users = users.filter(u=>u.socketId !== socketId);
 }
 
+
+const findFriend = (id) =>{
+  return users.find(u=>u.userId === id);
+}
+
 io.on('connection', (socket) => {
 
   console.log(socket.id + " socket running")
 
   socket.on("addUserLive", (userId, userInfo) => {
+    
     addUser(userId,socket.id,userInfo);
     io.emit('getUser',users);
-  
+
+  });
+
+  socket.on("sendMessage", (data) => {
+    const user = findFriend(data.reseverId);
+   
+    if(user !== undefined){
+     
+        socket.to(user.socketId).emit('getMessage',data)
+    }
    
   });
+
+  socket.on('typingMessage',(data)=>{
+
+    const user = findFriend(data.reseverId);
+
+    if(user !== undefined){
+        
+        socket.to(user.socketId).emit('typingMessageGet',{
+            senderId : data.senderId,
+            reseverId : data.reseverId,
+            msg : data.msg
+        })
+    }
+});
   
- 
+
   socket.on('disconnect',()=>{
     console.log('user disconnect....');
     userRemove(socket.id);
