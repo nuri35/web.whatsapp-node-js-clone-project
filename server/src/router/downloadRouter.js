71776a242617e2dda2edd66b31451b46d4d0596a
Nurettin Şen <nurie487@gmail.com>
2022-05-06 20:03:27 +0300

    Delete router

--- server/src/router/downloadRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const downloadController = require("../controller/downloadController")
-
-
-
-router.post("/",downloadController.dowloadFile)
-
-
-module.exports=router
