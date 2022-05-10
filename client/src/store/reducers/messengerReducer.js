3ab762a399dd26a33c40a8949fe4390cf1d7611e
Nurettin Şen <nurie487@gmail.com>
2022-05-10 14:16:37 +0300

    Add if statement

--- client/src/store/reducers/messengerReducer.js
+++ client/src/store/reducers/messengerReducer.js
@@ -1,5 +1,5 @@
 
-import {FRIENDS_GET_SUCCESS,MESSAGE_GET_SUCCESS,MESSAGE_SEND_SUCCESS} from "../types/messengerType";
+import {FRIENDS_GET_SUCCESS,MESSAGE_GET_SUCCESS,MESSAGE_SEND_SUCCESS,UPDATE_FRIEND_MESSAGE,MESSAGE_SEND_SUCCESS_CLEAR,SEEN_MESSAGE,DELIVARED_MESSAGE,UPDATE,MESSAGE_GET_SUCCESS_CLEAR} from "../types/messengerType";
 
 
 
@@ -8,7 +8,7 @@ const messengerState = {
     message : [],
     messageSendSuccess : false,
     message_get_success : false,
-   
+    new_user_add : ''
 }
 
 
@@ -41,6 +41,70 @@ export const messengerReducer = (state=messengerState , action) =>{
         return {
             ...state,
             message : [...state.message,payload.message]
+        }
+    }
+
+    if(type === UPDATE_FRIEND_MESSAGE){
+        const index = state.friends.findIndex(f=>f.fndInfo._id === payload.msgInfo.reseverId || f.fndInfo._id === payload.msgInfo.senderId );
+        state.friends[index].msgInfo = payload.msgInfo;
+        state.friends[index].msgInfo.status = payload.status;
+        return state;
+
+    }
+    if(type === MESSAGE_SEND_SUCCESS_CLEAR){
+        return {
+            ...state,
+            messageSendSuccess : false
+        }
+    }
+
+    if(type === SEEN_MESSAGE){
+        const index = state.friends.findIndex(f=>f.fndInfo._id === payload.msgInfo.reseverId || f.fndInfo._id === payload.msgInfo.senderId );
+        state.friends[index].msgInfo.status = 'seen';
+        return {
+            ...state
+        };
+    }
+
+    if(type === DELIVARED_MESSAGE){
+        const index = state.friends.findIndex(f=>f.fndInfo._id === payload.msgInfo.reseverId || f.fndInfo._id === payload.msgInfo.senderId );
+        state.friends[index].msgInfo.status = 'delivared';
+        return {
+            ...state
+        };
+    }
+    if(type === UPDATE){
+        const index = state.friends.findIndex(f=>f.fndInfo._id === payload.id);
+        if(state.friends[index].msgInfo){
+            state.friends[index].msgInfo.status = 'seen';
+        }
+        return {
+            ...state
+        }
+    }
+    if(type === MESSAGE_GET_SUCCESS_CLEAR){
+        return {
+            ...state,
+            message_get_success : false
+        }
+    }
+    if(type === 'SEEN_ALL'){
+        const index = state.friends.findIndex(f=>f.fndInfo._id === payload.reseverId);
+        state.friends[index].msgInfo.status = 'seen';
+        return {
+            ...state
+        }
+    }
+    if(type === 'NEW_USER_ADD'){
+        return{
+            ...state,
+            new_user_add : payload.new_user_add
+        }
+    }
+    if(type === 'NEW_USER_ADD_CLEAR'){
+        return {
+            ...state,
+            new_user_add : ''
         }
     }
 
